c16b7de9b008d9ac0d63d9b2b3efeb6912721d84
Bobby Naluz <bnaluz9@gmail.com>
2025-04-27 13:05:49 -0400

    test build

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,20 @@
 import styles from './page.module.css';
 
 export default function page() {
-  return (
-    <div className={styles['hero-container']}>
-      <div className={styles['hero-top-text']}>
-        m<span style={{ fontWeight: 'bold' }}>BOLD</span>en CHANGE
-      </div>
-      <video className={styles['hero-video']} autoPlay loop muted playsInline>
-        <source src="/coming-soon.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-      <div className={styles['hero-text']}>
-        <h1>Coming soon</h1>
-        <p>We are working hard to bring you our new site. Stay tuned!</p>
-      </div>
-    </div>
-  );
+  <div>Test</div>;
+  // return (
+  //   <div className={styles['hero-container']}>
+  //     <div className={styles['hero-top-text']}>
+  //       m<span style={{ fontWeight: 'bold' }}>BOLD</span>en CHANGE
+  //     </div>
+  //     <video className={styles['hero-video']} autoPlay loop muted playsInline>
+  //       <source src="/coming-soon.mp4" type="video/mp4" />
+  //       Your browser does not support the video tag.
+  //     </video>
+  //     <div className={styles['hero-text']}>
+  //       <h1>Coming soon</h1>
+  //       <p>We are working hard to bring you our new site. Stay tuned!</p>
+  //     </div>
+  //   </div>
+  // );
 }
