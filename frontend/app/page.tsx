dfc49245b244125151bfeeccd48248dcad5338ab
Bobby Naluz <bnaluz9@gmail.com>
2025-04-27 13:04:34 -0400

    rebuild

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import styles from './page.module.css';
 
-export default function Home() {
+export default function page() {
   return (
     <div className={styles['hero-container']}>
       <div className={styles['hero-top-text']}>
