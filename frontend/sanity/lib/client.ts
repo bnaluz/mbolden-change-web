3125312555307acce8fb6f4e116053cb5852552c
Bobby Naluz <bnaluz9@gmail.com>
2025-05-04 21:23:12 -0400

    sanity-typegen

---
+++ frontend/sanity/lib/client.ts
@@ -0,0 +1,13 @@
+import { createClient } from 'next-sanity';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const token = process.env.SANITY_API_READ_TOKEN;
+
+export const client = createClient({
+  projectId: projectId,
+  dataset,
+  apiVersion: '2025-03-04',
+  useCdn: true,
+  token,
+});
