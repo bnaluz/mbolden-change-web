9dd3ece2782a8b80f90072713991c6909a5bb3fd
Bobby Naluz <bnaluz9@gmail.com>
2025-05-05 22:28:33 -0400

    test dataset val

--- frontend/sanity/lib/client.ts
+++ frontend/sanity/lib/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from 'next-sanity';
 
 import { projectId, dataset } from '../env';
+console.log(dataset);
 
 // const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 // const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
@@ -8,7 +9,7 @@ const token = process.env.SANITY_API_READ_TOKEN;
 
 export const client = createClient({
   projectId,
-  dataset,
+  dataset: dataset,
   apiVersion: '2025-03-04',
   useCdn: true,
   token,
