044c1371a97ba17ccca04b818a14baba66f86a56
Bobby Naluz <bnaluz9@gmail.com>
2025-05-05 22:14:21 -0400

    test

--- frontend/lib/getPage.ts
+++ frontend/lib/getPage.ts
@@ -4,7 +4,7 @@ import { PAGE_QUERY } from '@/sanity/lib/queries';
 import { Page } from '@/sanity/types';
 
 export const getPage = cache(async (slug: string) => {
-  const page = await client.fetch<Page>(PAGE_QUERY, { slug });
+  const page = await client.fetch(PAGE_QUERY, { slug });
 
   if (!page) {
     return null;
