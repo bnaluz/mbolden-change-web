56d56eb8dcb173e8a9196c8047d4c11f7047acc7
Bobby Naluz <bnaluz9@gmail.com>
2025-05-05 22:31:09 -0400

    final test of the night

--- frontend/sanity/lib/client.ts
+++ frontend/sanity/lib/client.ts
@@ -1,15 +1,14 @@
 import { createClient } from 'next-sanity';
 
-import { projectId, dataset } from '../env';
-console.log(dataset);
+import { projectId } from '../env';
 
 // const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-// const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const token = process.env.SANITY_API_READ_TOKEN;
 
 export const client = createClient({
   projectId,
-  dataset: dataset,
+  dataset,
   apiVersion: '2025-03-04',
   useCdn: true,
   token,
