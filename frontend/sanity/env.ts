c33a28279033ff42c269c0621b44666dd3712574
Bobby Naluz <bnaluz9@gmail.com>
2025-05-05 22:23:02 -0400

    test dataset

--- frontend/sanity/env.ts
+++ frontend/sanity/env.ts
@@ -2,7 +2,7 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-04';
 
 export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  process.env.NEXT_PUBLIC_SANITY_DATASET!,
   'Missing environment variable NEXT_PUBLIC_SANITY_DATASET'
 );
 
