7c58abd71cd95315a81c3ed8d44cc1837cddcce4
Maazin Sherif <maazinsherif99@gmail.com>
2025-05-09 12:30:57 -0400

    add: structure and styling to Hero

--- frontend/components/blocks/Hero.tsx
+++ frontend/components/blocks/Hero.tsx
@@ -1,6 +1,9 @@
 import { Hero as HeroType } from '@/sanity/types';
-import { PortableTextBlock } from 'next-sanity';
 import SanityNextImage from '../SanityNextImage';
+import Grid from '../Grid';
+import GridItem from '../GridItem';
+import Headline from '../atoms/Headline';
+import { PortableTextBlock } from 'next-sanity';
 import PortableTextComponent from '../PortableTextComponent';
 import ButtonComponent from '../atoms/ButtonComponent';
 import { resolveHref } from '@/lib/resolveHref';
@@ -11,26 +14,37 @@ export default function Hero({ title, text, image, button }: HeroType) {
 
   return (
     <section className={styles.heroSection}>
-      {image ? (
-        <div>
-          <SanityNextImage image={image} fit="cover" />
-        </div>
-      ) : null}
-      {title ? <h1>{title}</h1> : null}
-      <div>
-        {text ? (
-          <PortableTextComponent value={text as PortableTextBlock[]} />
+      <Grid className={styles.grid}>
+        <GridItem desktopSpan={7} mobileSpan={12} desktopOffset={1}>
+          <div className={styles.textSection}>
+            {title && <Headline tag="h1" text={title} className={styles.headline}/>}
+            {text && (
+              <div className={styles.pText}>
+                <PortableTextComponent value={text as PortableTextBlock[]} />
+              </div>
+            )}
+            {button?.label && (
+              <ButtonComponent
+              className={styles.button}
+                href={resolveHref(button)}
+                isExternal={!!button.externalLink}
+                variant="primary"
+              >
+                {button.label}
+              </ButtonComponent>
+            )}
+          </div>
+        </GridItem>
+
+        {image ? (
+          <GridItem desktopSpan={5} mobileSpan={12}>
+            <div className={styles.imageSection}>
+              <SanityNextImage image={image} fit="cover" />
+            </div>
+          </GridItem>
         ) : null}
-        {button?.label && (
-          <ButtonComponent
-            href={resolveHref(button)}
-            isExternal={!!button.externalLink}
-            variant="primary"
-          >
-            {button.label}
-          </ButtonComponent>
-        )}
-      </div>
+
+      </Grid>
     </section>
   );
 }
