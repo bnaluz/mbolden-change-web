ec9baf48e6f96ac22547940aa9ac9f5a2cd9fe80
Maazin Sherif <maazinsherif99@gmail.com>
2025-05-11 15:34:42 -0400

    fix: full screen styling and mobile responsiveness

--- frontend/components/blocks/Hero.tsx
+++ frontend/components/blocks/Hero.tsx
@@ -1,48 +1,41 @@
 import { Hero as HeroType } from '@/sanity/types';
 import SanityNextImage from '../SanityNextImage';
-import Grid from '../Grid';
-import GridItem from '../GridItem';
 import Headline from '../atoms/Headline';
 import { PortableTextBlock } from 'next-sanity';
 import PortableTextComponent from '../PortableTextComponent';
 import ButtonComponent from '../atoms/ButtonComponent';
 import { resolveHref } from '@/lib/resolveHref';
-
-import styles from './Hero.module.css'
+import styles from './Hero.module.css';
 
 export default function Hero({ title, text, image, button }: HeroType) {
-
   return (
     <section className={styles.heroSection}>
-      <Grid className={styles.grid}>
-        <GridItem desktopSpan={5} mobileSpan={12} desktopOffset={1}>
-          <div className={styles.textSection}>
-            {title && <Headline tag="h1" text={title} className={styles.headline}/>}
-            {text && (
-              <div className={styles.pText}>
-                <PortableTextComponent value={text as PortableTextBlock[]} />
-              </div>
-            )}
-            {button?.label && (
-              <ButtonComponent
+      <div className={styles.heroGrid}>
+        <div className={styles.textSection}>
+          {title && <Headline tag="h1" text={title} className={styles.headline} />}
+          {text && (
+            <div className={styles.pText}>
+              <PortableTextComponent value={text as PortableTextBlock[]} />
+            </div>
+          )}
+          {button?.label && (
+            <ButtonComponent
               className={styles.button}
-                href={resolveHref(button)}
-                isExternal={!!button.externalLink}
-                variant="primary"
-              >
-                {button.label}
-              </ButtonComponent>
-            )}
+              href={resolveHref(button)}
+              isExternal={!!button.externalLink}
+              variant="primary"
+            >
+              {button.label}
+            </ButtonComponent>
+          )}
+        </div>
+
+        {image && (
+          <div className={styles.imageSection}>
+            <SanityNextImage image={image} fit="cover" className={styles.heroImage} />
           </div>
-        </GridItem>
-
-        {image ? (
-          <GridItem desktopSpan={7} mobileSpan={12}>
-              <SanityNextImage image={image} fit="contain" className='styles.heroImage'/>
-          </GridItem>
-        ) : null}
-
-      </Grid>
+        )}
+      </div>
     </section>
   );
 }
