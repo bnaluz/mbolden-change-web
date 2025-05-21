f846687f6d9e73e08f7e791fcc61a5b5f9539c57
bnaluz <112456971+bnaluz@users.noreply.github.com>
2025-05-20 23:48:30 -0700

    Feat/footer refresh (#24)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -9,8 +9,6 @@ export default async function Page() {
     return <div>Page not found</div>;
   }
 
-  console.log(page);
-
   return (
     <div className={styles['page']}>
       {page.content && <PageBuilder content={page.content} />}
